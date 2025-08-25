9f103e3d8822323bbe51824c30271dd0c085f034
Raghabendra Dash <raghabendradash779@gmail.com>
2025-08-25 20:00:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const app = express();
-const db = require('./db');
 require('dotenv').config();
+const db = require('./db');
 const passport = require('./auth');
+const app = express();
 
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); // req.body
